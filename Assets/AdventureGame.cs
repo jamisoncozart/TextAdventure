ef6e7232dd5693e6f03b8a1f7a36c0371f17be92
Jamison Cozart <jamisoncozart@gmail.com>
2019-12-25 15:08:58 -0800

    Manage player input and next state

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -21,6 +21,20 @@ public class AdventureGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //ManageState();
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        } 
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        textComponent.text = state.GetStateStory();
     }
 }
